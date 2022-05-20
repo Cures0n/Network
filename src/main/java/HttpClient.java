47df54747206685fd2f42eecad9915125524ee5a
Климов Иван <klimov.ie@elt.cor>
2022-05-20 15:10:41 +0700

    add project

---
+++ src/main/java/HttpClient.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class HttpClient {
+
+    public static void main(String... args) {
+
+        String  host = "localhost";
+        int port = 8081;
+
+        try (Socket clSocket  = new Socket(host,port);
+             PrintWriter out = new PrintWriter(clSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clSocket.getInputStream()))){
+            out.println("GET/HTTP/1.1\n" +
+                    "Host: localhost\n\n\n");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
